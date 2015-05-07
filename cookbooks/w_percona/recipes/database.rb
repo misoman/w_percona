7e7f4936c1af12db263a8763c589ba23a63bab05
Joel Handwell <joelhandwell@gmail.com>
2015-05-07 16:51:15 -0400

    Upgrade percona cookbook

--- cookbooks/w_percona/recipes/database.rb
+++ cookbooks/w_percona/recipes/database.rb
@@ -1,9 +1,27 @@
 root_password   = node['percona']['server']['root_password']
 db_host         = node['hostname'].downcase
 
+## security config
+# clean up empty user
+[db_host, 'localhost'].each do |empty_user_host|
+  execute "delete default anonymous user @#{empty_user_host}" do
+    command "mysql -uroot -p'#{root_password}' -e \"DELETE FROM mysql.user WHERE user='' AND host='#{empty_user_host}';\""
+    action :run
+  end
+end
+
+# apply root password on all hosts
+[db_host, '192.168.33.1', '127.0.0.1', '::1'].each do |root_host|
+  execute "apply root password on @#{root_host}" do
+    command "mysql -uroot -p'#{root_password}' -e \"UPDATE mysql.user SET password=password('#{root_password}') WHERE user='root' AND host='#{root_host}';\""
+    action :run
+  end
+end
+
 node['w_common']['web_apps'].each do |web_app|
 
-  webapp_host     = web_app['webapp_db_connection']['webapp_domain']
+  vhost = web_app['vhost']['main_domain']
+  webapp_host     = web_app['connection_domain']['webapp_domain']
 
   Chef::Log.info("web_app['mysql'].class: #{web_app['mysql'].class}")
 
@@ -25,41 +43,30 @@ node['w_common']['web_apps'].each do |web_app|
     webapp_username = database['user']
     webapp_password = database['password']
 
-    ## security config
-    # clean up empty user
-    [db_host, 'localhost'].each do |empty_user_host|
-      execute "delete default anonymous user @#{empty_user_host}" do
-        command "mysql -uroot -p'#{root_password}' -e \"DELETE FROM mysql.user WHERE user='' AND host='#{empty_user_host}';\""
-        action :run
-      end
-    end
-
-    # apply root password on all hosts
-    [db_host, '192.168.33.1', '127.0.0.1', '::1'].each do |root_host|
-      execute "apply root password on @#{root_host}" do
-        command "mysql -uroot -p'#{root_password}' -e \"UPDATE mysql.user SET password=password('#{root_password}') WHERE user='root' AND host='#{root_host}';\""
-        action :run
-      end
-    end
-
     ## webapp related config
-    execute "Create a mysql database for webapp" do
+    execute "Create a mysql database #{webapp_db} for webapp #{vhost}" do
       command "mysql -uroot -p'#{root_password}' -e \"CREATE DATABASE IF NOT EXISTS #{webapp_db};\""
       action :run
     end
 
-    [webapp_host, 'localhost'].each do |webapp_user_host|
-      execute "Create a mysql user for webapp if not exist, and grant access to the user" do
+    webapp_hosts = []
+
+    node['dbhosts']['webapp_ip'].each_index do |index|
+      webapp_hosts << index.to_s + web_app['connection_domain']['webapp_domain']
+    end
+
+    webapp_hosts << 'localhost'
+
+    webapp_hosts.each do |webapp_user_host|
+      execute "Create a mysql user for webapp if not exist, and grant access of #{webapp_db} to user #{webapp_username} at host #{webapp_user_host} for vhost #{vhost}" do
         command "mysql -uroot -p'#{root_password}' -e \"GRANT ALL ON #{webapp_db}.* TO '#{webapp_username}'@'#{webapp_user_host}' IDENTIFIED BY '#{webapp_password}';\""
         action :run
       end
     end
-
-    execute 'flush privileges' do
-      command "mysqladmin -uroot -p'#{root_password}' reload"
-      action :run
-    end
-
   end
+end
 
+execute 'flush privileges' do
+  command "mysqladmin -uroot -p'#{root_password}' reload"
+  action :run
 end
--- cookbooks/w_percona/recipes/default.rb
+++ cookbooks/w_percona/recipes/default.rb
@@ -1,30 +1,9 @@
-# Setup the Percona XtraDB Cluster
 cluster_ips = []
 unless Chef::Config[:zero]
   search(:node, 'role:percona').each do |other_node|
     next if other_node['private_ipaddress'] == node['private_ipaddress']
     Chef::Log.info "Found Percona XtraDB cluster peer: #{other_node['private_ipaddress']}"
     cluster_ips << other_node['private_ipaddress']
-  end
-end
-
-cluster_ips.each do |ip|
-  firewall_rule "allow Percona group communication to peer #{ip}" do
-    source ip
-    port 4567
-    action :allow
-  end
-
-  firewall_rule "allow Percona state transfer to peer #{ip}" do
-    source ip
-    port 4444
-    action :allow
-  end
-
-  firewall_rule "allow Percona incremental state transfer to peer #{ip}" do
-    source ip
-    port 4568
-    action :allow
   end
 end
 
@@ -39,8 +18,12 @@ include_recipe 'percona::toolkit'
 include_recipe 'w_percona::database'
 include_recipe 'w_percona::xinetd' if node['percona']['xinetd_enabled']
 
+firewall 'ufw' do
+  action :enable
+end
+
 [3306, 4444, 4567, 4568, 9200].each do |percona_port|
-  firewall_rule 'percona' do
+  firewall_rule "percona port #{percona_port.to_s}" do
     port     percona_port
     protocol :tcp
     action   :allow
