8d3e6c383119b52ac2707bc6761e28ec37e8c76b
Full Of Lilies <fulloflilies@gmail.com>
2015-05-14 12:00:08 -0400

    fix db hostname

--- spec/w_percona/database_spec.rb
+++ spec/w_percona/database_spec.rb
@@ -16,7 +16,7 @@ describe 'w_percona::default' do
         node.set['w_common']['web_apps'] = web_apps
         node.set['dbhosts']['webapp_ip'] = ['1.1.1.1', '2.2.2.2']
         node.set['percona']['server']['root_password'] = 'rootpassword'
-        node.automatic['hostname'] = 'dbhost.example.com'
+        node.automatic['hostname'] = '0dbhost.example.com'
       end.converge(described_recipe)
     end
 
@@ -25,13 +25,13 @@ describe 'w_percona::default' do
       stub_search(:node, 'role:percona').and_return([ { private_ipaddress: '10.10.10.10' }, { private_ipaddress: '10.10.10.11' } ])
     end
 
-    ['dbhost.example.com', 'localhost'].each do |empty_user_host|
+    ['0dbhost.example.com', 'localhost'].each do |empty_user_host|
       it "delete default anonymous user @ #{empty_user_host}" do
         expect(chef_run).to run_execute("mysql -uroot -p'rootpassword' -e \"DELETE FROM mysql.user WHERE user='' AND host='#{empty_user_host}';\"")
       end
     end
 
-    ['dbhost.example.com', '192.168.33.1', '127.0.0.1', '::1'].each do |root_host|
+    ['0dbhost.example.com', '192.168.33.1', '127.0.0.1', '::1'].each do |root_host|
       it "apply root password on @#{root_host}" do
         expect(chef_run).to run_execute("mysql -uroot -p'rootpassword' -e \"UPDATE mysql.user SET password=password('rootpassword') WHERE user='root' AND host='#{root_host}';\"")
       end
