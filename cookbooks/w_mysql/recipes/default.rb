a5ea36c50b150050161edfe359488c9f590e4628
Full Of Lilies <fulloflilies@gmail.com>
2015-05-14 17:26:40 -0400

    Add database.rb recipe in w_mysql cookbook

--- cookbooks/w_mysql/recipes/default.rb
+++ cookbooks/w_mysql/recipes/default.rb
@@ -1,7 +1,12 @@
+root_password = data_bag_item('w_mysql', 'root_credential')['root_password']
+
 mysql_service 'default' do
   bind_address '0.0.0.0' 
   data_dir '/data/db'
+  initial_root_password root_password
   action [:create, :start]
 end
 
 mysql_client 'default'
+
+include_recipe 'w_mysql::database'
