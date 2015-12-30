38d96d95d14f9ba6ddda7fb118d4b43fd344dcd5
Joel Handwell <joelhandwell@gmail.com>
2015-12-30 22:32:20 +0900

    Chefspec test coverage 100%

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,6 @@
+default['percona']['apt']['keyserver'] = 'hkp://keyserver.ubuntu.com:80'
+default['percona']['server']['tmpdir'] = '/var/tmp'
+default['percona']['server']['slave_load_tmpdir'] = '/var/tmp'
 default['percona']['server']['binlog_format'] = 'ROW'
 default['percona']['xinetd_enabled'] = false
 default['percona']['cluster']['wsrep_sst_auth'] = 'sstdefaultuser:sstdefaultpassword'
--- spec/w_percona/default_spec.rb
+++ spec/w_percona/default_spec.rb
@@ -7,9 +7,14 @@ describe 'w_percona::default' do
     let(:web_apps) do
       [
         {vhost: {main_domain: 'example.com'}, connection_domain: { webapp_domain: 'webapp.example.com' }, mysql: [ { db: 'db1', user: 'user', password: 'pw' } ] },
-        {vhost: {main_domain: 'ex.com'}, connection_domain: { webapp_domain: 'webapp.example.com' }, mysql: [ { db: ['db2', 'db3', 'db4'], user: 'user', password: 'pw' } ] }
+        {vhost: {main_domain: 'ex.com'}, connection_domain: { webapp_domain: 'webapp.example.com' }, mysql: [ { db: ['db2', 'db3', 'db4'], user: 'user', password: 'pw' } ] },
+        {vhost: {main_domain: 'vhost-without-connectiondomain-and-mysql.com'}}
       ]
     end
+
+    let(:package_cluster) {chef_run.package('percona-xtradb-cluster-56')}
+    let(:conf_template) {chef_run.template('/etc/mysql/my.cnf')}
+    let(:execute_grants) {chef_run.execute('mysql-install-privileges')}
 
     let(:chef_run) do
       ChefSpec::SoloRunner.new do |node|
@@ -28,6 +33,8 @@ describe 'w_percona::default' do
       stub_command("grep 9200/tcp /etc/services").and_return(false)
       stub_command("mysqladmin --user=root --password='' version").and_return(true)
       stub_command("mysql -uroot -p'rootpassword' -e \"SELECT user FROM mysql.user where host='localhost' and user='clustercheck';\" | grep -c \"clustercheck\"").and_return(false)
+      stub_command("test -f /var/lib/mysql/mysql/user.frm").and_return(true)
+      stub_command("mysqladmin --user=root --password='' version").and_return(true)
     end
 
     it 'enables firewall' do
@@ -40,14 +47,57 @@ describe 'w_percona::default' do
       end
     end
 
-    %w( cluster backup toolkit ).each do |recipe|
-      it "runs recipe percona::#{recipe}" do
-        expect(chef_run).to include_recipe("percona::#{recipe}")
+    describe 'percona::cluster recipe' do
+      it 'include percona::cluster recipe' do
+        expect(chef_run).to include_recipe('percona::cluster')
       end
-    end
 
-    it 'creats main config file' do
-      expect(chef_run).to create_template('/etc/mysql/my.cnf').with(source: 'my.cnf.cluster.erb')
+      it 'include percona::package_repo recipe' do
+        expect(chef_run).to include_recipe('percona::package_repo')
+        expect(chef_run).to add_apt_preference('00percona').with(glob: '*', pin: 'release o=Percona Development Team', pin_priority: '1001')
+        expect(chef_run).to add_apt_repository('percona').with(uri: 'http://repo.percona.com/apt', distribution: 'trusty', components: ['main'], keyserver: 'hkp://keyserver.ubuntu.com:80', key: '0x1C4CBDCDCD2EFD2A')
+      end
+
+      it 'installs percona cluster package' do
+        expect(chef_run).to install_package('percona-xtradb-cluster-56')
+        expect(package_cluster).to notify('service[mysql]').to(:stop).immediately
+      end
+
+      it 'include percona::configure_server recipe' do
+        expect(chef_run).to include_recipe('percona::configure_server')
+        expect(chef_run).not_to install_package('libjemalloc1')
+        expect(chef_run).to create_template('/root/.my.cnf').with(owner: 'root', group: 'root', mode: '0600', source: 'my.cnf.root.erb', sensitive: true)
+        expect(chef_run).to create_directory('/etc/mysql').with(owner: 'root', group: 'root', mode: '0755')
+        expect(chef_run).to create_directory('/var/lib/mysql').with(owner: 'mysql', group: 'mysql', recursive: true)
+        expect(chef_run).to create_directory('log directory').with(path: '/var/log/mysql', owner: 'mysql', group: 'mysql', recursive: true)
+        expect(chef_run).to create_directory('/var/tmp').with(owner: 'mysql', group: 'mysql', recursive: true)
+        expect(chef_run).to create_directory('/etc/mysql/conf.d/').with(owner: 'mysql', group: 'mysql', recursive: true)
+        expect(chef_run).not_to create_directory('slow query log directory').with(path: '/var/log/mysql', owner: 'mysql', group: 'mysql', recursive: true)
+        expect(chef_run).to enable_service('mysql')
+        expect(chef_run).not_to run_execute('setup mysql datadir').with(command: 'mysql_install_db --defaults-file=/etc/mysql/my.cnf --user=mysql')
+        expect(chef_run).not_to include_recipe('percona::ssl')
+        expect(chef_run).to create_template('/etc/mysql/my.cnf').with(source: 'my.cnf.cluster.erb', owner: 'root', group: 'root', mode: '0644', sensitive: true)
+        expect(conf_template).to notify('service[mysql]').to(:restart).immediately
+        expect(chef_run).to run_execute('Update MySQL root password').with(command: "mysqladmin --user=root --password='' password 'rootpassword'", sensitive: true)
+        expect(chef_run).to create_template('/etc/mysql/debian.cnf').with(source: 'debian.cnf.erb', owner: 'root', group: 'root', mode: '0640', sensitive: true)
+      end
+
+      it 'include percona::access_grants recipe' do
+        expect(chef_run).to include_recipe('percona::access_grants')
+        expect(chef_run).to create_template('/etc/mysql/grants.sql').with(source: 'grants.sql.erb', owner: 'root', group: 'root', mode: '0600', sensitive: true)
+        expect(chef_run).not_to run_execute('mysql-install-privileges').with(command: "/usr/bin/mysql -p'rootpassword -e '' &> /dev/null > /dev/null &> /dev/null ; if [ $? -eq 0 ] ; then /usr/bin/mysql -p'rootpassword < /etc/mysql/grants.sql ; else /usr/bin/mysql < /etc/mysql/grants.sql ; fi ;")
+        expect(execute_grants).to subscribe_to('template[/etc/mysql/grants.sql]').on(:run).immediately
+      end
+
+      it 'include percona::backup recipe' do
+        expect(chef_run).to include_recipe('percona::backup')
+        expect(chef_run).to install_package('xtrabackup').with(options: '--force-yes')
+      end
+
+      it 'include percona::toolkit recipe' do
+        expect(chef_run).to include_recipe('percona::toolkit')
+        expect(chef_run).to install_package('percona-toolkit').with(options: '--force-yes')
+      end
     end
 
     it 'creats /etc/mysql/my.cnf' do
@@ -63,7 +113,5 @@ describe 'w_percona::default' do
     it 'not runs recipe w_percona::xinetd' do
       expect(chef_run).to include_recipe('w_percona::xinetd')
     end
-
-
   end
 end
