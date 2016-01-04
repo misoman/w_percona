a138503343a2b9824e0ce937bfed14e88a70c4d4
Joel Handwell <joelhandwell@gmail.com>
2016-01-04 16:11:06 +0900

    Refactor to use shared w_common web_apps attribute

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,3 +11,12 @@ RSpec.configure do |config|
   config.platform = 'ubuntu'
   config.version = '14.04'
 end
+
+def web_apps
+  [
+    { vhost: { main_domain: 'example.com', aliases: ['www.example.com', 'ex.com'], docroot: '/websites/example.com/www'  } , deploy: { repo_ip: '9.9.9.9', repo_domain: 'git.examplewebsite.com', repo_path: '/websites/example.com', repo_url: 'https://git.examplewebsite.com/www.git' }   , connection_domain: { webapp_domain: 'webapp.example.com', db_domain: 'db.example.com', varnish_domain: 'varnish.example.com' }, mysql: [ { db: 'db',                  user: 'user', password: 'password' }], varnish: { purge_target: true }  },
+    { vhost: { main_domain: 'example2.com',                                        docroot: '/websites/example2.com/sub' } , deploy: { repo_ip: '9.9.9.9', repo_domain: 'git.examplewebsite.com', repo_path: '/websites/example2.com', repo_url: 'https://git.examplewebsite.com/www2.git' } , connection_domain: { webapp_domain: 'webapp.example.com', db_domain: 'db.example.com'                                        }, mysql: [ { db: ['db2', 'db3', 'db4'], user: 'user', password: 'password' }]                                   },
+    { vhost: { main_domain: 'example3.com',                                        docroot: '/websites/example3.com/sub' }                                                                                                                                                                   , connection_domain: { webapp_domain: 'webapp.example.com', db_domain: 'db.example.com'                                        }, mysql: [ { db: ['db2', 'db3', 'db4'], user: 'user', password: 'password' }]                                   },
+    { vhost: { main_domain: 'docroot-only-vhost.com',                              docroot: '/websites/dov'              }}
+  ]
+end
--- spec/w_percona/database_spec.rb
+++ spec/w_percona/database_spec.rb
@@ -4,14 +4,6 @@ describe 'w_percona::database' do
 
   context 'with default setting' do
 
-    let(:web_apps) do
-      [
-        { vhost: {main_domain: 'example.com'}, connection_domain: { webapp_domain: 'webapp.example.com' }, mysql: [ { db: 'db1', user: 'user', password: 'pw' } ] },
-        { vhost: {main_domain: 'ex.com'}, connection_domain: { webapp_domain: 'webapp.example.com' }, mysql: [ { db: ['db2', 'db3', 'db4'], user: 'user', password: 'pw' } ] },
-        { vhost: {main_domain: 'vhost-without-connectiondomain-and-mysql.com'}}
-      ]
-    end
-
     let(:chef_run) do
       ChefSpec::SoloRunner.new do |node|
         node.set['w_common']['web_apps'] = web_apps
@@ -56,36 +48,41 @@ describe 'w_percona::database' do
     end
 
     it 'Create a mysql database for webapp' do
-      expect(chef_run).to run_execute("mysql -uroot -p'rootpassword' -e \"CREATE DATABASE IF NOT EXISTS db1;\"")
-      expect(chef_run).to run_execute("mysql -uroot -p'rootpassword' -e \"CREATE DATABASE IF NOT EXISTS db2;\"")
-      expect(chef_run).to run_execute("mysql -uroot -p'rootpassword' -e \"CREATE DATABASE IF NOT EXISTS db3;\"")
-      expect(chef_run).to run_execute("mysql -uroot -p'rootpassword' -e \"CREATE DATABASE IF NOT EXISTS db4;\"")
-    end
+      expect(chef_run).to run_execute("Create a mysql database db for webapp example.com").with(command: "mysql -uroot -p'rootpassword' -e \"CREATE DATABASE IF NOT EXISTS db;\"")
 
-    ['example.com', 'ex.com'].each do |vhost|
-      ['db1', 'db2', 'db3', 'db4'].each do |webapp_db|
-
-        webapp_hosts = []
-
-        webapp_hosts << '1.1.1.1'
-        webapp_hosts << '2.2.2.2'
-
-        ['1.1.1.1', '2.2.2.2'].each_index do |index|
-          webapp_hosts << index.to_s + 'webapp.example.com'
-        end
-
-        webapp_hosts << 'localhost'
-
-        webapp_hosts.each do |webapp_user_host|
-          it "Create a mysql user for webapp if not exist, and grant access of #{webapp_db} to user user at host #{webapp_user_host} for vhost #{vhost}" do
-            expect(chef_run).to run_execute("mysql -uroot -p'rootpassword' -e \"GRANT ALL ON #{webapp_db}.* TO 'user'@'#{webapp_user_host}' IDENTIFIED BY 'pw';\"")
-          end
+      ['example2.com', 'example3.com'].each do |vhost|
+        ['db2', 'db3', 'db4'].each do |webapp_db|
+          expect(chef_run).to run_execute("Create a mysql database #{webapp_db} for webapp #{vhost}").with(command: "mysql -uroot -p'rootpassword' -e \"CREATE DATABASE IF NOT EXISTS #{webapp_db};\"")
         end
       end
     end
 
-    it 'create user previleges for db1 for 0webapp.example.com' do
-      expect(chef_run).to run_execute("mysql -uroot -p'rootpassword' -e \"GRANT ALL ON db1.* TO 'user'@'0webapp.example.com' IDENTIFIED BY 'pw';\"")
+    webapp_hosts = []
+
+    webapp_hosts << '1.1.1.1'
+    webapp_hosts << '2.2.2.2'
+
+    ['1.1.1.1', '2.2.2.2'].each_index do |index|
+      webapp_hosts << index.to_s + 'webapp.example.com'
+    end
+
+    webapp_hosts << 'localhost'
+
+    webapp_hosts.each do |webapp_user_host|
+
+      it "Create a mysql user for webapp if not exist, and grant access of db to user user at host #{webapp_user_host} for vhost example.com" do
+        expect(chef_run).to run_execute("Create a mysql user for webapp if not exist, and grant access of db to user user at host #{webapp_user_host} for vhost example.com")
+        .with(command: "mysql -uroot -p'rootpassword' -e \"GRANT ALL ON db.* TO 'user'@'#{webapp_user_host}' IDENTIFIED BY 'password';\"")
+      end
+
+      ['example2.com', 'example3.com'].each do |vhost|
+        ['db2', 'db3', 'db4'].each do |webapp_db|
+          it "Create a mysql user for webapp if not exist, and grant access of #{webapp_db} to user user at host #{webapp_user_host} for vhost #{vhost}" do
+            expect(chef_run).to run_execute("Create a mysql user for webapp if not exist, and grant access of #{webapp_db} to user user at host #{webapp_user_host} for vhost #{vhost}")
+            .with(command: "mysql -uroot -p'rootpassword' -e \"GRANT ALL ON #{webapp_db}.* TO 'user'@'#{webapp_user_host}' IDENTIFIED BY 'password';\"")
+          end
+        end
+      end
     end
 
     it 'flush privileges' do
